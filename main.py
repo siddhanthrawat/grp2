29a3dcc4305a1ac2a60a8763a4c516f8d0e5c74f
siddhanthrawat <104669068+siddhanthrawat@users.noreply.github.com>
2025-05-12 10:56:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -77,12 +77,9 @@ last_sequence = np.expand_dims(last_sequence, axis=0)
 predicted_scaled = model.predict(last_sequence)
 predicted_values = scaler.inverse_transform(predicted_scaled)[0]
 
-# Calculate the average value of each symptom over the last 'window_size' days
-average_values = data[symptoms].iloc[-window_size:].mean()
-
-# Alert check: if predicted value is 1.5 times higher than the average historical value for that symptom
+# Alert check: if predicted value is 120% higher than the last actual value
 alert_triggered = any(
-    predicted_values[i] > average_values[symptoms[i]] * 1.5
+    predicted_values[i] > data[symptoms[i]].iloc[-1] * 1.2
     for i in range(len(symptoms))
 )
 
